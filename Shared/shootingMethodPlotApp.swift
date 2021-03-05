c5c51995d78662b59eabab2584b3c40e4030b242
Matt <madas@hawk.iit.edu>
2021-03-05 17:34:57 -0600

    Initial Commit

---
+++ Shared/shootingMethodPlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  shootingMethodPlotApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct shootingMethodPlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
